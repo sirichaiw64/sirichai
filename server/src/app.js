355f42123690420721df37141c7dd31770eaab0b
sirichaiw64 <sirichaiw64@nu.ac.th>
2023-08-16 22:05:52 +0700

    page 28

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 require('./route')(app)
 
 app.get('/status', function (req,res){
-    res.send('Hello nodejs server belong to Nattapong')
+    res.send('Hello nodejs server belong to sirichai')
 })
 
 app.get('/hello/:name', function(req,res){
