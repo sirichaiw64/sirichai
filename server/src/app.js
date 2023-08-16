77c40d2e5c398a53fa603db3985494b7a8e35e48
sirichaiw64 <sirichaiw64@nu.ac.th>
2023-08-16 20:16:52 +0700

    finished ch4

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,12 @@
 let express = require('express')
+let bodyParser = require('body-parser')
+
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.get('/status', function (req,res){
+
     res.send('Hello nodejs server belong to Sirichai')
 })
 
@@ -9,6 +14,29 @@ app.get('/hello/:name', function(req,res){
     console.log('hello - ' + req.params.name)
     res.send('say hello with ' + req.params.name)
 })
+
+app.get('/user/:userId',function(req,res){
+res.send('ดูข้อมูลผู้ใช้'+req.params.userId)
+})
+
+app.get('/users',function(req,res){
+    res.send('ดูข้อมูลผู้ใช้ทั้งหมด')
+    })
+
+app.post('/user',function (req,res){
+    res.send('สร้างผู้ใช้'+JSON.stringify(req.body))
+})
+
+app.put('/user/:userId',function (req,res){
+    res.send('เเก้ไขข้อมูลผู้ใช้'+ req.params.userId + " " + JSON.stringify(req.body))
+})
+
+app.delete('/user/:userId',function (req,res){
+    res.send('ลบผู้ใช้งาน' + req.params.userId + " " + JSON.stringify(req.body.name))
+})
+
+
+
 
 let port = 8081
 app.listen(port, function(){
