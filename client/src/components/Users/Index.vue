331b175976e9cc75edda79f76db64022616b07f4
sirichaiw64 <sirichaiw64@nu.ac.th>
2023-09-28 00:29:55 +0700

    page 147

--- client/src/components/Users/Index.vue
+++ client/src/components/Users/Index.vue
@@ -1,32 +1,49 @@
 <template>
     <div>
-        <h2>Get all users</h2>
-        <h4>จํานวนผู้ใช้งาน {{ users.length }}</h4>
-        <div v-for="user in users" v-bind:key="user.id">
-            <p>id: {{ user.id }}</p>
-            <p>ชืEอ-นามสกุล: {{ user.name }} - {{ user.lastname }}</p>
-            <p>email: {{ user.email }}</p>
-            <p>password: {{ user.password }}</p>
-            <p><button>ดูข้อมูลผู้ใช้</button></p>
-        </div>
+      <h2>Get all users</h2>
+      <h4>จํานวนผู้ใช้งาน {{ users.length }}</h4>
+      <p><button v-on:click="navigateTo('/user/create')">สร้างผู้ใช้งาน</button></p>
+      <div v-for="user in users" v-bind:key="user.id">
+        <p>id: {{ user.id }}</p>
+        <p>ชือ-นามสกุล: {{ user.name }} - {{ user.lastname }}</p>
+        <p>email: {{ user.email }}</p>
+        <p>password: {{ user.password }}</p>
+        <p><button v-on:click="navigateTo('/user/' + user.id)">ดูข้อมูลผู้ใช้</button>
+          <button v-on:click="navigateTo('/user/edit/' + user.id)">แก้ไขข้อมูล</button>
+          <button v-on:click="deleteUser(user)">ลบข้อมูล</button>
+        </p>
+  
+        <hr>
+      </div>
     </div>
-</template>
-<script>
-import UsersService from '@/Services/usersservices'
-export default {
+  </template> 
+  <script>
+  import UsersService from '@/services/UsersService'
+  export default {
     data() {
-        return {
-            users: []
-        }
+      return {
+        users: []
+      }
     },
     async created() {
-        this.users = (await UsersService.index()).data
+      this.users = (await UsersService.index()).data
     },
     methods: {
-        navigateto(route) {
-            this.$router.push(route)
+      navigateTo(route) {
+        this.$router.push(route)
+      },
+      async deleteUser(user) {
+        try {
+          await UsersService.delete(user)
+          this.refreshData()
+        } catch (err) {
+          console.log(err)
         }
+      },
+      async refreshData() {
+        this.users = (await UsersService.index()).data
+      }
     }
-}
-</script>
-<style scoped></style>
+  }
+  </script> 
+  <style scoped></style>
--- client/src/components/Users/ShowUser.vue
+++ client/src/components/Users/ShowUser.vue
@@ -1,28 +1,26 @@
-<template>
-    <div>
-        <h1>Show User</h1>
-        <p>id: {{ user.id }}</p>
-        <p>ชืEอ-นามสกุล: {{ user.name }} - {{ user.lastname }}</p>
-        <p>email: {{ user.email }}</p>
-        <p>password: {{ user.password }}</p>
-    </div>
-</template>
-<script>
-import UsersService from '@/services/usersservices'
-export default {
-    data() {
-        return {
-            user: null
-        }
-    },
-    async created () {
- try {
-let userId = this.$route.params.userId
-this.user = (await UsersService.show(userId)).data
- } catch (error) {
-console.log (error)
- } 
-}
-}
-</script>
-<style scoped></style>
+<template> 
+    <div> 
+    <h1>Show User</h1> 
+    <p>id: {{ user.id }}</p> 
+    <p>ชือ-นามสกุล: {{ user.name }} - {{ user.lastname }}</p> 
+    <p>email: {{ user.email }}</p> 
+    <p>password: {{ user.password }}</p>
+    </div> 
+  </template> 
+  <script> 
+  import UsersService from '@/services/UsersService' 
+   
+  export default { 
+    data () { 
+      return { 
+        user:  []
+      } 
+    }, 
+    async created () { 
+      let userId = this.$route.params.userId 
+      this.user = (await UsersService.show(userId)).data 
+    } 
+  } 
+  </script> 
+  <style scoped> 
+  </style>
